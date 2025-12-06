c959abab9fc5032391e028b7c777f9075ad49899
TahaNadeemKhan <tahak6884@gmail.com>
2025-12-06 17:21:30 +0500

    feat: update favicon to use project logo

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Native Book',
   tagline: 'Physical AI & Humanoid Robotics Documentation',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
